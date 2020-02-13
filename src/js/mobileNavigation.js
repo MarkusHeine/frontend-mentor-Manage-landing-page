007ab2d8f6d9bb42727a44c3cb47398ce8a23854
MarkusHeine <markusheine.dev@gmail.com>
2020-02-13 18:34:51 +0100

    refactor css and add content

--- src/js/mobileNavigation.js
+++ src/js/mobileNavigation.js
@@ -1,20 +1,25 @@
 export const mobileNavigation = () => {
     const hamburgerBtn = document.querySelector("img[id='hamburger-btn']");
-    const mobileLinks = document.querySelector(".mobile-links");
-    const body = document.querySelector("body");
+    const mobileLinksWrapper = document.querySelector(".mobile-links-wrapper");
 
     let navOpen = false;
+    console.log(hamburgerBtn);
 
-    hamburgerBtn.addEventListener("click", () => {
+    const toggleMobileNav = () => {
+        console.log("click");
+        console.log(hamburgerBtn);
+
         navOpen = !navOpen;
+        console.log("navOpen", navOpen);
         if (!navOpen) {
             hamburgerBtn.src = "./assets/images/icons/icon-hamburger.svg";
-            mobileLinks.style.display = "none";
-            body.classList.remove("body-bg-mobile");
+            mobileLinksWrapper.style.display = "none";
         } else {
             hamburgerBtn.src = "./assets/images/icons/icon-close.svg";
-            mobileLinks.style.display = "flex";
-            body.classList.add("body-bg-mobile");
+            mobileLinksWrapper.style.display = "flex";
         }
-    });
+        console.log(hamburgerBtn);
+    };
+
+    hamburgerBtn.addEventListener("click", () => toggleMobileNav());
 };
