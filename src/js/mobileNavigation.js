4c13d66bc40bd399acca698efff37fa9fdb5a28a
MarkusHeine <markusheine.dev@gmail.com>
2020-02-14 08:59:00 +0100

    fix the mobile nav bug

--- src/js/mobileNavigation.js
+++ src/js/mobileNavigation.js
@@ -1,25 +1,29 @@
 export const mobileNavigation = () => {
-    const hamburgerBtn = document.querySelector("img[id='hamburger-btn']");
+    const hamburgerBtn = document.querySelector("#hamburger-btn");
     const mobileLinksWrapper = document.querySelector(".mobile-links-wrapper");
+    const mobileLinksList = document.querySelector(".mobile-links-list");
+    const body = document.querySelector("body");
 
     let navOpen = false;
-    console.log(hamburgerBtn);
 
     const toggleMobileNav = () => {
-        console.log("click");
-        console.log(hamburgerBtn);
-
         navOpen = !navOpen;
-        console.log("navOpen", navOpen);
         if (!navOpen) {
             hamburgerBtn.src = "./assets/images/icons/icon-hamburger.svg";
             mobileLinksWrapper.style.display = "none";
+            body.classList.remove("no-scroll");
         } else {
             hamburgerBtn.src = "./assets/images/icons/icon-close.svg";
             mobileLinksWrapper.style.display = "flex";
+            body.classList.add("no-scroll");
         }
-        console.log(hamburgerBtn);
     };
 
-    hamburgerBtn.addEventListener("click", () => toggleMobileNav());
+    hamburgerBtn.addEventListener("click", () => {
+        toggleMobileNav();
+    });
+
+    mobileLinksList.addEventListener("click", () => {
+        toggleMobileNav();
+    });
 };
